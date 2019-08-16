ce5ca79810f394e3fe0748a8b3039d0d548df468
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-16 23:06:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  arr = []
+  let arr = []
   for (i = 0; i < musicians.length; i++) {
     arr.push(musicians[i] + ' plays ' + instruments[i]);
   }
