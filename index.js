c2161a6b1eb902c706907014b16bb5032bce2c80
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-16 23:05:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // add solution here
+arr = [];
+
 function theBeatlesPlay(musicians, instruments) {
-  arr = []
-  for (i = 0; i < musicians.length; i++) {
-    arr.push(musicians[i] + ' plays ' + instruments[i]);
-  }
+  
 }
